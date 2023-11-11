3d9665723ad00d563947e90eef4d949cd8331a2a
IRatajczyk <igor@ratajczyk.eu>
2023-11-11 16:30:08 +0100

    fitness function corrected

--- EvolutionaryAlgorithm/src/fitness_function.jl
+++ EvolutionaryAlgorithm/src/fitness_function.jl
@@ -68,27 +68,41 @@ function Penalty(individual::Individual)::Float64
 end
 
 function Intersection(individual::Individual, i::Int64, j::Int64)::Float64
+    λ::Float64 = .5*√(2)
     intersection::Float64 = 0
     if (individual.x[i] - individual.x[j])^2 + (individual.y[i] - individual.y[j])^2 < 2
         x::Float64 = (individual.x[i] - individual.x[j])
         y::Float64 = (individual.y[i] - individual.y[j])
-        t::Float64 = individual.theta[j] - individual.theta[i]
-        projected_x:: Vector{Float64} = x .+ 0.5 * [sin(t + π/4), sin(t - π/4), sin(t + 3π/4), sin(t - 3π/4)] * √(2)
-        projected_y:: Vector{Float64} = y .+ 0.5 * [cos(t + π/4), cos(t - π/4), cos(t + 3π/4), cos(t - 3π/4)] * √(2)
-        projected_x_:: Vector{Float64} = -x .+ 0.5 * [sin(-t + π/4), sin(-t - π/4), sin(-t + 3π/4), sin(-t - 3π/4)] * √(2)
-        projected_y_:: Vector{Float64} = -y .+ 0.5 * [cos(-t + π/4), cos(-t - π/4), cos(-t + 3π/4), cos(-t - 3π/4)] * √(2)
 
-        if Intersect(projected_x) && Intersect(projected_y) && Intersect(projected_x_) && Intersect(projected_y_)
-            intersection += IntersectionArea(projected_x, projected_y)*IntersectionArea(projected_x_, projected_y_)
+        θ::Float64 = individual.theta[i]
+        ω::Float64 = individual.theta[j]
+
+        Px::Vector{Float64} = x .+ λ * [sin(θ + π/4), sin(θ - π/4), sin(θ + 3π/4), sin(θ - 3π/4)]
+        Py::Vector{Float64} = y .+ λ * [cos(θ + π/4), cos(θ - π/4), cos(θ + 3π/4), cos(θ - 3π/4)]
+        Pxy::Matrix{Float64} = R(ω) * [Px'; Py']
+
+        
+        Rx::Vector{Float64} = -x .+ λ * [sin(ω + π/4), sin(ω - π/4), sin(ω + 3π/4), sin(ω - 3π/4)]
+        Ry::Vector{Float64} = -y .+ λ * [cos(ω + π/4), cos(ω - π/4), cos(ω + 3π/4), cos(ω - 3π/4)]
+        Rxy::Matrix{Float64} = R(θ) * [Rx'; Ry']
+
+        if Intersect(Pxy[1,:]) && Intersect(Pxy[2,:]) && Intersect(Rxy[1,:]) && Intersect(Rxy[2,:])
+            intersection += IntersectionArea(Pxy) * IntersectionArea(Rxy)
         end
     end
     return intersection
+end
+
+function R(θ::Float64)::Matrix{Float64}
+    [[cos(θ) -sin(θ)]; [sin(θ) cos(θ)]]
 end
 
 function Intersect(v::Vector{Float64})::Bool
     return ((minimum(v) < 0.5) && (maximum(v) > 0.5)) || ((minimum(v) < -0.5) && (maximum(v) > -0.5))
 end
 
-function IntersectionArea(x::Vector{Float64}, y::Vector{Float64})::Float64
+function IntersectionArea(u::Matrix{Float64})::Float64
+    x::Vector{Float64} = u[1,:]
+    y::Vector{Float64} = u[2,:]
     return (min(0.5, maximum(x)) - max(-0.5, minimum(x)))*(min(0.5, maximum(y)) - max(-0.5, minimum(y)))
 end
